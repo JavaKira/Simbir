add6b13480bce6945fb06b6117cad93d83171e9d
JavaKira <Javakira@yandex.ru>
2023-10-09 23:02:14 +0300

    feat: adds AccountController POST /singUp, POST /singOut, PUT /update

--- src/main/java/com/github/javakira/simbir/account/AccountController.java
+++ src/main/java/com/github/javakira/simbir/account/AccountController.java
@@ -8,11 +8,26 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
     @GetMapping(value = "/Me", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody String me() {
-        return "hello";
+        return "";
     }
 
     @PostMapping(value = "/SingIn", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody String singIn() {
+        return "";
+    }
+
+    @PostMapping(value = "/SingUp", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String singUp() {
+        return "";
+    }
+
+    @PostMapping(value = "/SingOut", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String singOut() {
+        return "";
+    }
+
+    @PutMapping(value = "/Update", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String update() {
         return "";
     }
 }
