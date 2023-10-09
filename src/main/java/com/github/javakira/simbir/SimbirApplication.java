82cc31dfb61edeb8bdf7244b561356f15bfdaaea
JavaKira <Javakira@yandex.ru>
2023-10-09 19:40:02 +0300

    fix: change Java version to 17 & add exclude DataSourceAutoConfiguration for SpringBootApplication

--- src/main/java/com/github/javakira/simbir/SimbirApplication.java
+++ src/main/java/com/github/javakira/simbir/SimbirApplication.java
@@ -2,12 +2,11 @@ package com.github.javakira.simbir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SimbirApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimbirApplication.class, args);
     }
-
 }
