8292fe7a4b966571812e162b8cd688db69148f2c
JavaKira <Javakira@yandex.ru>
2023-10-12 17:17:34 +0300

    feat: adds some todo in RentController

--- src/main/java/com/github/javakira/simbir/rent/RentController.java
+++ src/main/java/com/github/javakira/simbir/rent/RentController.java
@@ -9,34 +9,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/Rent")
 public class RentController {
+    //todo написать алгоритм поиска
     @GetMapping("/Transport")
     public ResponseEntity<Transport> search(@RequestBody RentSearchParams params) {
         return ResponseEntity.ok(null);
     }
-
+    //todo ограничения: Только арендатор и владелец транспорта
+    //todo написать класс Rent и сделать его репозиторий. у Rent должен быть RentType и тд фигня
     @GetMapping("{rentId}")
     public ResponseEntity<Rent> rentInfo(@PathVariable Long rentId) {
         return ResponseEntity.ok(null);
     }
-
+    //todo добавить в Account историю аренд
     @GetMapping("/MyHistory")
     public ResponseEntity<List<Rent>> rentHistory() {
         return ResponseEntity.ok(null);
     }
-
+    //todo граничения: Только владелец этого транспорта
+    //todo добавить в Transport историю аренд
     @GetMapping("/TransportHistory/{transportId}")
     public ResponseEntity<?> transportHistory(@PathVariable Long transportId) {
         return ResponseEntity.ok(null);
     }
-
+    //todo Только авторизованные пользователи, нельзя брать в аренду собственный транспорт
     @PostMapping("/New/{transportId}")
     public ResponseEntity<?> rent(@PathVariable Long transportId, @RequestBody NewRentRequest request) {
         return ResponseEntity.ok(null);
     }
 
-    //todo ограничения: только человек который создавал эту аренду
+    //todo ограничения: только человек который создавал эту аренду. У Rent должен быть Account owner
     @PostMapping("/End/{rentId}")
-    public ResponseEntity<?> end(@PathVariable Long rentId, @RequestBody NewRentRequest request) {
+    public ResponseEntity<?> end(@PathVariable Long rentId, @RequestBody RentEndRequest request) {
         return ResponseEntity.ok(null);
     }
 }
