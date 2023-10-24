fd31cdbf1e4b859b1f3d0d5b39e91027c0548f4e
JavaKira <Javakira@yandex.ru>
2023-10-24 16:59:36 +0300

    refactor: refactor payment package

--- src/main/java/com/github/javakira/simbir/payment/PaymentService.java
+++ src/main/java/com/github/javakira/simbir/payment/PaymentService.java
@@ -4,6 +4,8 @@ import com.github.javakira.simbir.account.Account;
 import com.github.javakira.simbir.account.AccountRepository;
 import com.github.javakira.simbir.account.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,26 +15,33 @@ import java.util.Optional;
 public class PaymentService {
     private final AccountRepository accountRepository;
 
-    public void addMoney(Long accountId, Long userId) {
+    public ResponseEntity<?> hesoyam(long accountId, long userId) {
         Optional<Account> account = accountRepository.findById(accountId);
         if (account.isEmpty())
-            throw new IllegalArgumentException("Account with id %d doesnt exist".formatted(userId));
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Account with id %d doesnt exist".formatted(accountId));
 
-        if (userId.equals(accountId)) {
+        if (userId == accountId) {
             account.get().setMoney(account.get().getMoney() + 250_000);
             accountRepository.save(account.get());
-            return;
+            return new ResponseEntity<>(HttpStatus.OK);
         }
 
         Optional<Account> user = accountRepository.findById(userId);
         if (user.isEmpty())
-            throw new IllegalArgumentException("Account with id %d doesnt exist".formatted(userId));
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Account with id %d doesnt exist".formatted(userId));
 
         if (user.get().getRole() == Role.admin) {
             account.get().setMoney(account.get().getMoney() + 250_000);
             accountRepository.save(account.get());
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
-            throw new IllegalArgumentException("Only admin can replenish money for other accounts");
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Only admin can replenish money for other accounts");
         }
     }
 }
