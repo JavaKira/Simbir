5a44a526638f570099a616c15a7dd532e74bfab9
JavaKira <Javakira@yandex.ru>
2023-10-29 17:15:09 +0300

    feat: I lazy to comment this commit

--- src/main/java/com/github/javakira/simbir/admin/transport/RegisterTransportByAdminRequest.java
+++ src/main/java/com/github/javakira/simbir/admin/transport/RegisterTransportByAdminRequest.java
@@ -1,25 +1,30 @@
 package com.github.javakira.simbir.admin.transport;
 
 import com.github.javakira.simbir.transport.TransportType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterTransportByAdminRequest {
-    private long ownerId;
-    private boolean canBeRented;
+    @NonNull
+    private Long ownerId;
+    @NonNull
+    private Boolean canBeRented;
+    @NonNull
     private TransportType transportType;
+    @NonNull
     private String model;
+    @NonNull
     private String color;
+    @NonNull
     private String identifier;
-    private String description; //todo may be null
-    private double latitude;
-    private double longitude;
-    private Double minutePrice; //todo may be null
-    private Double dayPrice; //todo may be null
+    private String description;
+    @NonNull
+    private Double latitude;
+    @NonNull
+    private Double longitude;
+    private Double minutePrice;
+    private Double dayPrice;
 }
